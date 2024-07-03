81fb19b35b186a3c992c08d110b967053f6da95a
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-07-03 11:43:52 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask_restful import Api, Resource
 
 from models import db, Plant
 
-app = Flask(name)
+app = Flask(__name__)  # Provide the import_name argument
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -16,53 +16,58 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
 
- def get(self):
-    plants = [plant.to_dict() for plant in Plant.query.all()]
-    return make_response(jsonify(plants), 200)
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
 
-def post(self):
-    data = request.get_json()
+    def post(self):
+        data = request.get_json()
 
-    new_plant = Plant(
-        name=data['name'],
-        image=data['image'],
-        price=data['price'],
-    )
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
 
-    db.session.add(new_plant)
-    db.session.commit()
+        db.session.add(new_plant)
+        db.session.commit()
 
-    return make_response(new_plant.to_dict(), 201)
+        return make_response(new_plant.to_dict(), 201)
+
+
 api.add_resource(Plants, '/plants')
 
+
 class PlantByID(Resource):
+
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        data = request.get_json()
 
-def patch(self, id):
-    data = request.get_json()
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
 
-    plant = Plant.query.filter_by(id=id).first()
+        db.session.commit()
 
-    for attr in data:
-        setattr(plant, attr, data[attr])
+        return make_response(jsonify(plant.to_dict()), 200)
 
-    db.session.add(plant)
-    db.session.commit()
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        db.session.delete(plant)
+        db.session.commit()
 
-    return make_response(plant.to_dict(), 200)
+        return make_response('', 204)
 
-def delete(self, id):
-    plant = Plant.query.filter_by(id=id).first()
-    db.session.delete(plant)
-    db.session.commit()
 
-    return make_response('', 204)
 api.add_resource(PlantByID, '/plants/<int:id>')
 
-if name == 'main':
-    app.run(port=5555, debug=True)
 
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
